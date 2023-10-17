271716ee7d6306eca1bb3c947094e1bdc37b89af
Pusaka Manggala <pusakamanggala@gmail.com>
2023-10-17 15:07:43 +0700

    chore : set query client options refetchOnWindowFocus to false

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,13 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { UserProvider } from "./context/UserContext.jsx";
 import { ToastContainer } from "react-toastify";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
