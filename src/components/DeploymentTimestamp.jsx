1ee67f73e6ab233a9d1d15ed114321626aa1e204
Pusaka Manggala <pusakamanggala@gmail.com>
2023-10-17 15:05:07 +0700

    refactor: Rearrange table columns

--- src/components/DeploymentTimestamp.jsx
+++ src/components/DeploymentTimestamp.jsx
@@ -1,68 +1,61 @@
 import PropTypes from "prop-types";
+import { formatTimestamp, getBadgeColor } from "../utils/helper";
 
 const DeploymentTimestamp = ({ timestamp }) => {
-  const getStatusColor = (status) => {
-    switch (status) {
-      case "complete":
-        return "bg-green-100 text-green-600";
-      case "on-delivery":
-        return "bg-blue-100 text-blue-600";
-      case "failed":
-        return "bg-red-100 text-red-600";
-      case "pending":
-        return "bg-yellow-100 text-yellow-600";
-    }
-  };
   return (
     <section className="px-5 border-2 rounded-2xl w-full overflow-y-auto">
       <table className="border-separate border-spacing-y-5 text-sm w-full font-medium">
         <thead>
           <tr className="h-10 align-top text-gray-500 divide-y-reverse divide-y-2">
-            <th className="text-start pr-6 font-semibold border-b-2 w-20">
-              Timestamp
+            <th className="text-start pr-6 font-semibold border-b-2 whitespace-nowrap">
+              Last Timestamp
             </th>
-            <th className="font-semibold px-6 ">Developer</th>
-            <th className="font-semibold px-6">Repository</th>
-            <th scope="col" className="font-semibold px-6">
-              Branch
+            <th className="font-semibold text-start px-6 w-60">Pod Name</th>
+            <th className="font-semibold text-start px-6 w-40">Namespace</th>
+            <th scope="col" className="font-semibold text-start px-6 w-40">
+              Reason
             </th>
-            <th scope="col" className="font-semibold pl-10 pr-16">
-              Image
+            <th scope="col" className="font-semibold text-start px-6 w-64">
+              Message
             </th>
-            <th className="font-semibold pr-6 pl-2 w-20 whitespace-nowrap text-start">
-              Deploy At
-            </th>
-            <th className="font-semibold w-40">Status</th>
+            <th className="font-semibold w-40 px-6">Type</th>
           </tr>
         </thead>
         <tbody>
-          {timestamp.map((timestampData) => (
-            <tr className="text-center" key={timestampData.id}>
+          {timestamp.map((timestampData, index) => (
+            <tr className="text-center" key={index}>
               <td className="text-start">
-                <p className="line-clamp-2">{timestampData.timestamp}</p>
+                <p className="line-clamp-2 w-20 hover:line-clamp-none">
+                  {formatTimestamp(timestampData.lastTimestamp)}
+                </p>
               </td>
-              <td>
-                <p className="line-clamp-2">{timestampData.developer}</p>
+              <td className="text-start px-6">
+                <p className="line-clamp-2 hover:line-clamp-none">
+                  {timestampData.podName}
+                </p>
               </td>
-              <td>
-                <p className="line-clamp-1">{timestampData.repository}</p>
+              <td className="text-start px-6">
+                <p className="line-clamp-1 hover:line-clamp-none">
+                  {timestampData.namespace}
+                </p>
               </td>
-              <td>
-                <p className="line-clamp-1">{timestampData.branch}</p>
+              <td className="text-start px-6">
+                <p className="line-clamp-1 hover:line-clamp-none">
+                  {timestampData.reason}
+                </p>
               </td>
-              <td>
-                <p className="line-clamp-1">{timestampData.image}</p>
+              <td className="text-start px-6">
+                <p className="line-clamp-2 hover:line-clamp-none">
+                  {timestampData.message}
+                </p>
               </td>
-              <td className="text-start pl-2">
-                <p className="line-clamp-2">{timestampData.deploy_at}</p>
-              </td>
-              <td className="capitalize px-5 whitespace-nowrap">
+              <td className="capitalize px-6 whitespace-nowrap">
                 <h1
-                  className={`${getStatusColor(
-                    timestampData.status
+                  className={`${getBadgeColor(
+                    timestampData.type
                   )} p-2 rounded-md`}
                 >
-                  {timestampData.status}
+                  {timestampData.type}
                 </h1>
               </td>
             </tr>
