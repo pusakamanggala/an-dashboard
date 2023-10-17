afecc4dcfb705b1e911c1aa4a2717781dfed012c
Pusaka Manggala <pusakamanggala@gmail.com>
2023-10-17 15:01:47 +0700

    feat : custom hooks to fetch dashboard content

---
+++ src/hooks/useGetDashboardContent.jsx
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+import { getToken } from "../utils/helper";
+
+async function fetchDashboardContent(content) {
+  const token = getToken(); // Get the token from the utility function
+  const headers = {
+    "auth-token": token,
+  };
+  const response = await axios.get(
+    `${import.meta.env.VITE_API_ENDPOINT}dashboard/kube/main/${content}`,
+    {
+      headers,
+    }
+  );
+  return response.data;
+}
+
+export function useGetDashboardContent(content) {
+  return useQuery(
+    ["dashboardContent", content],
+    () => fetchDashboardContent(content),
+    {
+      enabled: !!getToken(),
+    }
+  );
+}
