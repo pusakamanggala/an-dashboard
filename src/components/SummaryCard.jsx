0254f8dc152a63f076e37aea9dc72e1eced6d2fe
Pusaka Manggala <pusakamanggala@gmail.com>
2023-10-17 15:03:11 +0700

    feat : add new data into card with conditional rendering

--- src/components/SummaryCard.jsx
+++ src/components/SummaryCard.jsx
@@ -6,6 +6,11 @@ const SummaryCard = ({
   value,
   readyInstanceValue,
   diskPressureValue,
+  runningInstanceValue,
+  pendingInstanceValue,
+  memoryPressureValue,
+  pidPressureValue,
+  networkUnavailableValue,
 }) => {
   return (
     <div className="border-2 p-4 space-y-6 rounded-md">
@@ -18,33 +23,125 @@ const SummaryCard = ({
           {value}
         </h1>
         <div className="font-medium text-xs 2xl:text-sm">
-          <div className="flex items-center space-x-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="8"
-              height="8"
-              viewBox="0 0 10 10"
-            >
-              <circle cx="5" cy="5" r="5" fill="green" />
-            </svg>
-            <p className="text-gray-400">
-              <span className="text-green-600">{readyInstanceValue}</span> Ready
-            </p>
-          </div>
-          <div className="flex items-center space-x-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="8"
-              height="8"
-              viewBox="0 0 10 10"
-            >
-              <circle cx="5" cy="5" r="5" fill="red" />
-            </svg>
-            <p className="text-gray-400">
-              <span className="text-red-600">{diskPressureValue}</span> Disk
-              Pressure
-            </p>
-          </div>
+          {readyInstanceValue && (
+            <div className="flex items-center space-x-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="8"
+                height="8"
+                viewBox="0 0 10 10"
+                className="fill-green-600"
+              >
+                <circle cx="5" cy="5" r="5" />
+              </svg>
+              <p className="text-gray-400">
+                <span className="text-green-600">{readyInstanceValue}</span>{" "}
+                Ready
+              </p>
+            </div>
+          )}
+          {runningInstanceValue > 0 && (
+            <div className="flex items-center space-x-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="8"
+                height="8"
+                viewBox="0 0 10 10"
+                className="fill-green-600"
+              >
+                <circle cx="5" cy="5" r="5" />
+              </svg>
+              <p className="text-gray-400">
+                <span className="text-green-600">{runningInstanceValue}</span>{" "}
+                Running
+              </p>
+            </div>
+          )}
+          {memoryPressureValue > 0 && (
+            <div className="flex items-center space-x-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="8"
+                height="8"
+                viewBox="0 0 10 10"
+                className="fill-red-600"
+              >
+                <circle cx="5" cy="5" r="5" />
+              </svg>
+              <p className="text-gray-400">
+                <span className="text-red-600">{memoryPressureValue}</span>{" "}
+                Memory Pressure
+              </p>
+            </div>
+          )}
+          {pidPressureValue > 0 && (
+            <div className="flex items-center space-x-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="8"
+                height="8"
+                viewBox="0 0 10 10"
+                className="fill-red-600"
+              >
+                <circle cx="5" cy="5" r="5" />
+              </svg>
+              <p className="text-gray-400">
+                <span className="text-red-600">{pidPressureValue}</span> Pid
+                Pressure
+              </p>
+            </div>
+          )}
+          {networkUnavailableValue > 0 && (
+            <div className="flex items-center space-x-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="8"
+                height="8"
+                viewBox="0 0 10 10"
+                className="fill-red-600"
+              >
+                <circle cx="5" cy="5" r="5" />
+              </svg>
+              <p className="text-gray-400">
+                <span className="text-red-600">{networkUnavailableValue}</span>{" "}
+                Network Unavailable
+              </p>
+            </div>
+          )}
+          {diskPressureValue > 0 && (
+            <div className="flex items-center space-x-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="8"
+                height="8"
+                viewBox="0 0 10 10"
+                className="fill-red-600"
+              >
+                <circle cx="5" cy="5" r="5" />
+              </svg>
+              <p className="text-gray-400">
+                <span className="text-red-600">{diskPressureValue}</span> Disk
+                Pressure
+              </p>
+            </div>
+          )}
+          {pendingInstanceValue > 0 && (
+            <div className="flex items-center space-x-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="8"
+                height="8"
+                viewBox="0 0 10 10"
+                className="fill-yellow-400"
+              >
+                <circle cx="5" cy="5" r="5" />
+              </svg>
+              <p className="text-gray-400">
+                <span className="text-yellow-400">{pendingInstanceValue}</span>{" "}
+                Pending
+              </p>
+            </div>
+          )}
         </div>
       </div>
     </div>
@@ -57,6 +154,11 @@ SummaryCard.propTypes = {
   value: PropsTypes.number.isRequired,
   readyInstanceValue: PropsTypes.number.isRequired,
   diskPressureValue: PropsTypes.number.isRequired,
+  runningInstanceValue: PropsTypes.number.isRequired,
+  pendingInstanceValue: PropsTypes.number.isRequired,
+  memoryPressureValue: PropsTypes.number.isRequired,
+  pidPressureValue: PropsTypes.number.isRequired,
+  networkUnavailableValue: PropsTypes.number.isRequired,
 };
 
 export default SummaryCard;
