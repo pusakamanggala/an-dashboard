9ee7e21f8226b3ad1b23d089b518e01c5677667b
Pusaka Manggala <pusakamanggala@gmail.com>
2023-10-17 14:51:03 +0700

    feat : new helper function to format timestamp and get badge color

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -10,3 +10,25 @@ export const getToken = () => {
     return null; // You can return null or any other suitable value.
   }
 };
+
+export const formatTimestamp = (timestamp) => {
+  const date = new Date(timestamp);
+
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0"); // Month is 0-indexed
+  const year = String(date.getFullYear());
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+
+  return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
+};
+
+export const getBadgeColor = (status) => {
+  switch (status) {
+    case "Normal":
+      return "bg-green-100 text-green-600";
+    case "Warning":
+      return "bg-red-100 text-red-600";
+  }
+};
