97e0d14a472d6033122cf2c3d787887fb58dfecb
Pusaka Manggala <pusakamanggala@gmail.com>
2023-10-27 17:33:44 +0700

    feat : API integration in services page and its components

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -2,56 +2,84 @@ import BoxIcon from "../icons/box-blue.svg";
 import UserIcon from "../icons/user-blue.svg";
 import GitlabIcon from "../icons/gitlab-blue.svg";
 import HealthIcon from "../icons/health-blue.svg";
-import LinkIcon from "../icons/link-blue.svg";
+import DocumentIcon from "../icons/document-blue.svg";
+import LinkICon from "../icons/link-blue.svg";
+import { getBadgeColor } from "../utils/helper";
 import PropTypes from "prop-types";
 
 const ServiceCard = ({ serviceData }) => {
-  const getStatusColor = (status) => {
-    switch (status) {
-      case "complete":
-        return "bg-green-100 text-green-600";
-      case "on-delivery":
-        return "bg-blue-100 text-blue-600";
-      case "failed":
-        return "bg-red-100 text-red-600";
-      case "pending":
-        return "bg-yellow-100 text-yellow-600";
-    }
-  };
-
   return (
     <div className="p-5 shadow-[0_3px_10px_0_rgba(0,0,0,0.25)] rounded-lg space-y-4 font-medium">
       <div className="flex gap-2 items-center">
-        <img className="w-5 h-5" src={BoxIcon} alt="" />
+        <img
+          className="w-5 h-5"
+          src={BoxIcon}
+          alt="service_name_icon"
+          title="service name"
+        />
         <p className="line-clamp-1">{serviceData.serviceName}</p>
       </div>
       <div className="flex gap-2 items-center">
-        <img className="w-5 h-5" src={UserIcon} alt="" />
-        <p className="line-clamp-1">{serviceData.developerName}</p>
+        <img
+          className="w-5 h-5"
+          src={UserIcon}
+          alt="project_owner_icon"
+          title="project owner"
+        />
+        <p className="line-clamp-1">{serviceData.projectOwner}</p>
       </div>
       <div className="flex gap-2 items-center">
-        <img className="w-5 h-5" src={GitlabIcon} alt="" />
-        <p className="line-clamp-1">{serviceData.images}</p>
-      </div>
-      <div className="flex gap-2 items-center">
-        <img className="w-5 h-5" src={HealthIcon} alt="" />
-        <p
-          className={`line-clamp-1 ${getStatusColor(
-            serviceData.status
-          )} px-2 py-1 capitalize rounded-md`}
-        >
-          {serviceData.status}
+        <img
+          className="w-5 h-5"
+          src={GitlabIcon}
+          alt="images_icon"
+          title="images"
+        />
+        <p className="line-clamp-1  hover:line-clamp-none">
+          {serviceData.images}
         </p>
       </div>
       <div className="flex gap-2 items-center">
-        <img className="w-5 h-5" src={LinkIcon} alt="" />
-        <p className="line-clamp-1">{serviceData.domain}</p>
+        <img
+          className="w-5 h-5"
+          src={HealthIcon}
+          alt="status_icon"
+          title="pod status"
+        />
+        <p
+          className={`line-clamp-1 ${getBadgeColor(
+            serviceData.podStatus
+          )} px-2 py-1 capitalize rounded-md`}
+        >
+          {serviceData.podStatus}
+        </p>
+      </div>
+      <div className="flex gap-2 items-center">
+        <img
+          className="w-5 h-5"
+          src={DocumentIcon}
+          alt="namespace_icon"
+          title="namespace"
+        />
+        <p className="line-clamp-1">{serviceData.namespace}</p>
+      </div>
+      <div className="flex gap-2 items-center">
+        <img
+          className="w-5 h-5"
+          src={LinkICon}
+          alt="namespace_icon"
+          title="ingress host"
+        />
+        <p className="line-clamp-2 text-sm  hover:line-clamp-none">
+          {serviceData.ingressHost}
+        </p>
       </div>
       <div className="flex justify-center">
         <a
           className="bg-sky-600 hover:bg-sky-800 transition-colors duration-300 text-white px-4 py-2 rounded-md"
-          href="go_to_app_link"
+          href={serviceData.ingressHost}
           target="_blank"
+          rel="noreferrer"
         >
           Go to the app
         </a>
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,58 +1,81 @@
 import React from "react";
 import ServiceCard from "../components/ServiceCard";
 import usePageTitle from "../hooks/usePageTitle";
+import { useGetServices } from "../hooks/useGetServices";
 
 const Services = () => {
-  //service dummy data
-  const servicesData = [
-    {
-      serviceName: "Manual Books",
-      developerName: "John Doe",
-      images: "pti-pratama-elf/dev-a-budi",
-      status: "on-delivery",
-      domain: "pti-pratama-elf/dev-a-budi",
-    },
-    {
-      serviceName: "Service 2",
-      developerName: "Jane Smith",
-      images: "example-service/dev-b-jane",
-      status: "on-delivery",
-      domain: "example-service/dev-b-jane",
-    },
-    {
-      serviceName: "Service 3",
-      developerName: "Alice Johnson",
-      images: "test-service/dev-c-alice",
-      status: "failed",
-      domain: "test-service/dev-c-alice",
-    },
-    {
-      serviceName: "Service 4",
-      developerName: "Bob Williams",
-      images: "custom-service/dev-d-bob",
-      status: "pending",
-      domain: "custom-service/dev-d-bob",
-    },
-    {
-      serviceName: "Service 5",
-      developerName: "Eve Anderson",
-      images: "prod-service/dev-e-eve",
-      status: "complete",
-      domain: "prod-service/dev-e-eve",
-    },
-  ];
+  const {
+    data: servicesData,
+    isLoading: servicesIsLoading,
+    isSuccess: servicesIsSuccess,
+    isError: servicesIsError,
+    error: servicesError,
+  } = useGetServices();
+
+  const cardLoadingSkeleton = () => {
+    return (
+      <div className="p-5 shadow-[0_3px_10px_0_rgba(0,0,0,0.25)] rounded-lg space-y-4 font-medium">
+        <div className="animate-pulse flex gap-2 items-center">
+          <div className="w-6 h-6 bg-gray-300 rounded-full"></div>
+          <div className="bg-gray-300 h-4 w-24 rounded-md"></div>
+        </div>
+        <div className="animate-pulse flex gap-2 items-center">
+          <div className="w-6 h-6 bg-gray-300 rounded-full"></div>
+          <div className="bg-gray-300 h-4 w-36 rounded-md"></div>
+        </div>
+        <div className="animate-pulse flex gap-2 items-center">
+          <div className="w-6 h-6 bg-gray-300 rounded-full"></div>
+          <div className="bg-gray-300 h-4 w-48 rounded-md"></div>
+        </div>
+        <div className="animate-pulse flex gap-2 items-center">
+          <div className="w-6 h-6 bg-gray-300 rounded-full"></div>
+          <div className="bg-gray-300 h-4 w-24 rounded-md"></div>
+        </div>
+        <div className="animate-pulse flex gap-2 items-center">
+          <div className="w-6 h-6 bg-gray-300 rounded-full"></div>
+          <div className="bg-gray-300 h-4 w-20 rounded-md"></div>
+        </div>
+        <div className="animate-pulse flex gap-2 items-center">
+          <div className="w-6 h-6 bg-gray-300 rounded-full"></div>
+          <div className="bg-gray-300 h-4 w-56 rounded-md"></div>
+        </div>
+        <div className="animate-pulse flex justify-center">
+          <div className="bg-gray-300 h-10 w-32 rounded-md"></div>
+        </div>
+      </div>
+    );
+  };
 
   const pageTitle = usePageTitle();
   return (
     <section className="space-y-5 w-full">
       <h1 className="font-semibold text-sky-700 text-2xl">{pageTitle}</h1>
-      <section className="grid md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-8 px-5">
-        {servicesData.map((service, index) => (
-          <React.Fragment key={index}>
-            <ServiceCard serviceData={service} />
-          </React.Fragment>
-        ))}
-      </section>
+      {servicesIsError && (
+        <p className="font-semibold text-center text-red-600">
+          {servicesError?.response?.data?.message || "Something went wrong"}
+        </p>
+      )}
+      {servicesIsSuccess && servicesData.data.length === 0 && (
+        <p>{servicesData.message}</p>
+      )}
+      {servicesIsSuccess && servicesData.data.length > 0 && (
+        <section className="grid md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-8 px-5">
+          {servicesData.data.map((service, index) => (
+            <React.Fragment key={index}>
+              <ServiceCard serviceData={service} />
+            </React.Fragment>
+          ))}
+        </section>
+      )}
+      {servicesIsLoading && (
+        <section className="grid md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-8 px-5">
+          {Array(2)
+            .fill()
+            .map(() => cardLoadingSkeleton())}
+
+          {cardLoadingSkeleton()}
+        </section>
+      )}
     </section>
   );
 };
